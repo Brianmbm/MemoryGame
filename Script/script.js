7fda6986afe7f44913f4a12637a8f77058a17cb7
Brian Bocquet <brianbocquet@gmail.com>
2023-12-05 03:58:16 +0100

    fixed round counter issue, optimized css, added buttons to win and lose screens

--- Script/script.js
+++ Script/script.js
@@ -1,5 +1,4 @@
-//TODO Prio Med: Game over view, add button to back to menu
-//TODO Prio Med: On win screen, add button to save highscore and button to go back to menu
+//TODO Prio Med: On win screen, add button to save highscore
 //TODO Prio Med: Show score during game
 //TODO Prio High: Add bonuses at end of round, show them in win screen
 //TODO Prio Low: Add click to continue dialogue in startscreen
@@ -11,7 +10,6 @@
 //FIXME Prio Low: can currently show more cards than 2 by clicking fast
 //FIXME Prio Low: Background image responsive but not in a good way.
 //FIXME Prio Low: Need time pause after last selection before game won/game over screen
-//Question: Shouldnt the cards be shown at start of round?
 
 const imagesLevelZero =['card_1.jpg', 'card_2.jpg', 'card_3.jpg', 'card_1.jpg', 'card_2.jpg', 'card_3.jpg']
 const imagesLevelOne =['card_1.jpg', 'card_2.jpg', 'card_3.jpg', 'card_4.jpg', 'card_1.jpg', 'card_2.jpg', 'card_3.jpg', 'card_4.jpg', 'card_5.jpg']
@@ -31,15 +29,13 @@ const columns = [columnOne, columnTwo, columnThree, columnFour];
 document.addEventListener('DOMContentLoaded', createStartScreen)
 
 function createStartScreen(){
-    const startscreenDiv = document.getElementById('start-screen');
+    const startscreenDiv = document.getElementById('start-screen')
     startscreenDiv.style.display = "flex"
     startscreenDiv.style.backgroundImage = `url('../../Resources/SplashscreenLoading.jpg')`
     //On click remove splash screen, add game board
     startscreenDiv.addEventListener('click', function () {
         startscreenDiv.style.display = "none"
-        createMenu()
-    })
-}
+        createMenu()})}
 
 function createMenu(){
     menuScreen.style.display = "flex"
@@ -48,11 +44,11 @@ function createMenu(){
     document.getElementById('menu-rect').style.marginTop="6vh"
     anime({
         targets: '#turning-circle',
-        rotate: '-360deg', // Rotate 360 degrees
-        duration: 2000,   // Animation duration in milliseconds
-        easing: 'linear', // Easing function (linear for a constant speed)
-        loop: true        // Loop the animation
-      });
+        rotate: '-360deg', 
+        duration: 2000,
+        easing: 'linear', 
+        loop: true});
+
     const startButton = document.getElementById('start-button')
     startButton.addEventListener('click', () => createLevelScreen())
     const guideButton = document.getElementById('guide-button')
@@ -62,8 +58,7 @@ function createMenu(){
     const highscoreButton = document.getElementById('highscore-button')
     highscoreButton.addEventListener('click', () => createHighScoreScreen())
     const quitButton = document.getElementById('quit-button')
-    quitButton.addEventListener('click', () => close())
-}
+    quitButton.addEventListener('click', () => close())}
 
 function createLevelScreen(){
     menuScreen.style.display='none'
@@ -74,8 +69,7 @@ function createLevelScreen(){
     const levelOneButton = document.getElementById('levelone-button')
     levelOneButton.addEventListener('click', () => createGameBoard(1))
     const levelTwoButton = document.getElementById('leveltwo-button')
-    levelTwoButton.addEventListener('click', () => createGameBoard(2))
-}
+    levelTwoButton.addEventListener('click', () => createGameBoard(2))}
 
 //Create intial game/gameboard
 function createGameBoard(level) {
@@ -84,27 +78,24 @@ function createGameBoard(level) {
     mainDiv.style.display = "flex"
     mainDiv.style.height="98vh"
     mainDiv.style.width="95vw"
-    let shuffledImages = shuffleCards(imagesByLevel[level]);
+    let shuffledImages = shuffleCards(imagesByLevel[level])
     let counter = 0
     //Clear columns from previous games
     for (let i = 0; i < columns.length; i++) {
-            columns[i].innerHTML = "";
+            columns[i].innerHTML = ""
     }
     const numColumns = level + 2
     for (let i = 0; i < numColumns; i++) {
-        columns[i].style.display = "flex";
+        columns[i].style.display = "flex"
         for (let j = 0; j < 3; j++) {
-            const cardDiv = document.createElement('div');
-            cardDiv.classList.add('card');
+            const cardDiv = document.createElement('div')
+            cardDiv.classList.add('card')
             const imageName = shuffledImages[counter++];
-            cardDiv.style.backgroundImage = `url('../../Resources/cardBack.jpg')`;
+            cardDiv.style.backgroundImage = `url('../../Resources/cardBack.jpg')`
             cardDiv.setAttribute('data-image', imageName);
-            cardDiv.addEventListener('click', () => toggleCard(cardDiv, level));
-            columns[i].appendChild(cardDiv);
-        }
-    }
+            cardDiv.addEventListener('click', () => toggleCard(cardDiv, level))
+            columns[i].appendChild(cardDiv)}}}
 
-}
 //Shuffle the cards before putting them in column (Fishe-Yates Shuffle, answer from Stackoverflow)
 let shuffleCards = (array) => {
     let currentIndex = array.length, randomIndex;
@@ -114,121 +105,124 @@ let shuffleCards = (array) => {
         [array[currentIndex], array[randomIndex]] = [
             array[randomIndex], array[currentIndex]]
     }
-    return array
-}
+    return array}
+
 //Show cards when clicked
 function toggleCard(cardDiv, level) {
     const isCardBack = cardDiv.style.backgroundImage.includes('cardBack')
     const imageName = cardDiv.getAttribute('data-image')
 
     if (isCardBack) {
-        cardDiv.style.backgroundImage = `url('../../Resources/${imageName}')`;
+        cardDiv.style.backgroundImage = `url('../../Resources/${imageName}')`
         selectedCards.push({ div: cardDiv, name: imageName });
 
         if (selectedCards.length === 2) {
             //Match Found
             if (selectedCards[0].name === selectedCards[1].name) {
                 setTimeout(() => {
-                    selectedCards.forEach((card) => card.div.remove());
+                    selectedCards.forEach((card) => card.div.remove())
                     selectedCards = [];
                     score += 50;
                     if(level === 0){
                         if (score >= 150) {
-                            createWinScreen();
-                        }
-                    }
+                            createWinScreen()}}
                     if(level === 1){
                         if (score >= 250) {
-                            createWinScreen();
-                        }
-                    }
+                            createWinScreen()}}
                     if(level === 2){
                         if (score >= 350) {
-                            createWinScreen();
-                        }
-                    }
+                            createWinScreen()}}
                     console.log(score)
                 }, 1000); 
             //Match not found
             } else {
                 setTimeout(() => {
                     selectedCards.forEach((card) => {
-                        card.div.style.backgroundImage = `url('../../Resources/cardBack.jpg')`;
-                    });
+                        card.div.style.backgroundImage = `url('../../Resources/cardBack.jpg')`});
                     selectedCards = [];
-                }, 1000); 
-            }
+                }, 1000);}
             roundCounter++;
             console.log(roundCounter)
-            if (++roundCounter % 3 === 0) {
-                addNewCards(level);
-            }
+            if (level === 0 && roundCounter % 3 === 0){
+                    addNewCards(level)}
+            else if (level === 1 && roundCounter % 5 === 0){
+                    addNewCards(level)}
+            else if (level === 2 && roundCounter % 7 === 0){
+                    addNewCards(level)}
+
             const numColumns = level + 2
             for (let i = 0; i < numColumns; i++) {
                 if (columns[i].childElementCount > 3) {
-                    createGameOverScreen();
-                    return; 
-                }
-            }
-        }
-    }
-}
+                    setTimeout(()=>{createGameOverScreen()}, 1000)
+                    return; }}}}}
+
 //Add one card to each column after three attempts
 function addNewCards(level) {
     const numColumns = level + 2
     for (let i = 0; i < numColumns; i++) {
         for (let j = 0; j < 1; j++) {
-            const cardDiv = document.createElement('div');
-            cardDiv.classList.add('card');
-            const imageName = getCardImage(level);
-            cardDiv.style.backgroundImage = `url('../../Resources/cardBack.jpg')`;
+            const cardDiv = document.createElement('div')
+            cardDiv.classList.add('card')
+            const imageName = getCardImage(level)
+            cardDiv.style.backgroundImage = `url('../../Resources/cardBack.jpg')`
             cardDiv.setAttribute('data-image', imageName);
-            cardDiv.addEventListener('click', () => toggleCard(cardDiv));
-            columns[i].appendChild(cardDiv);
-        }
-    }
-}
+            cardDiv.addEventListener('click', () => toggleCard(cardDiv))
+            columns[i].appendChild(cardDiv);}}}
+
 //Get random card to add (first from shuffled array)
 function getCardImage(level){
     let shuffledImages = shuffleCards(imagesByLevel[level]);
     const imageName = shuffledImages[0];
-    return imageName;
-}
+    return imageName;}
+
 function createWinScreen(){
     document.getElementById('main-game').style.display='none'
     const winScreen = document.getElementById('win-screen')
     winScreen.style.display = "flex"
     winScreen.style.backgroundImage = `url('../../Resources/SplashscreenWon.jpg')`
-}
+    winScreen.style.justifyContent = "end"
+    winbackButton = document.getElementById('winback-button')
+    winbackButton.style.alignSelf = "end"
+    winbackButton.style.margin = "2vh"
+    winbackButton.addEventListener('click', function () {
+        score = 0
+        winScreen.style.display = 'none'
+        menuScreen.style.display = "flex"})}
+
 function createGameOverScreen(){
     document.getElementById('main-game').style.display='none'
-    const gameoverScreen = document.getElementById('gameover-screen');
+    const gameoverScreen = document.getElementById('gameover-screen')
     gameoverScreen.style.display = "flex"
     gameoverScreen.style.backgroundImage = `url('../../Resources/SplashscreenGameOver.jpg')`
-}
+    gameoverScreen.style.justifyContent = "start"
+    losebackButton = document.getElementById('loseback-button')
+    losebackButton.style.alignSelf = "start"
+    losebackButton.style.margin = "2vh"
+    losebackButton.addEventListener('click', function () {
+        score = 0
+        gameoverScreen.style.display = 'none'
+        menuScreen.style.display = "flex"})}
+
 function createGuideScreen(){
     menuScreen.style.display='none'
     const guideScreen = document.getElementById('guide-screen')
     guideScreen.style.display = "flex"
     guideScreen.style.backgroundImage = `url('../../Resources/SplashscreenGuide.jpg')`
     document.getElementById('guide-back-button').addEventListener('click', function () {
-        guideScreen.style.display = 'none';
-        menuScreen.style.display = "flex"
-    });
-}
+        guideScreen.style.display = 'none'
+        menuScreen.style.display = "flex" })}
+
 function createAboutScreen(){
     menuScreen.style.display='none'
     const aboutScreen = document.getElementById('about-screen')
     aboutScreen.style.display = "flex"
     aboutScreen.style.backgroundImage = `url('../../Resources/SplashscreenAbout.jpg')`
     document.getElementById('about-back-button').addEventListener('click', function () {
-        aboutScreen.style.display = 'none';
-        menuScreen.style.display = "flex"
-    });
-}
+        aboutScreen.style.display = 'none'
+        menuScreen.style.display = "flex"})}
+
 function createHighScoreScreen(){
     menuScreen.style.display='none'
     const highScreen = document.getElementById('highscore-screen')
     highScreen.style.display = "flex"
-    highScreen.style.backgroundImage = `url('../../Resources/SplashscreenHigh.jpg')`
-}
+    highScreen.style.backgroundImage = `url('../../Resources/SplashscreenHigh.jpg')`}
